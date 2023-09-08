dd8c406ea545a4e26ebb6d061f09a4c8589f2fb6
Bruna_Midori <brunamidoriyy@yahoo.com.br>
2023-09-08 17:23:53 -0300

    card

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:trilhaapp/pages/dados_cadastrais.dart';
-import 'package:trilhaapp/pages/pagina1.dart';
+import 'package:trilhaapp/pages/card_page.dart';
 import 'package:trilhaapp/pages/pagina2.dart';
 import 'package:trilhaapp/pages/pagina3.dart';
 import 'package:trilhaapp/shared/widgets/custom_drawer.dart';
@@ -34,7 +33,7 @@ class _MainPageState extends State<MainPage> {
                     });
                   },
                   children: const [
-                    Pagina1Page(),
+                    CardPage(),
                     Pagina2Page(),
                     Pagina3Page(),
                   ]),
