d66ad5f4cd32099fc3ae5bc5363f01b7bb3e4e66
Bruna_Midori <brunamidoriyy@yahoo.com.br>
2023-09-08 16:03:52 -0300

    ShowModalBottomSheet

--- lib/shared/widgets/custom_drawer.dart
+++ lib/shared/widgets/custom_drawer.dart
@@ -10,15 +10,43 @@ class CustomDrawer extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          UserAccountsDrawerHeader(
-              decoration: BoxDecoration(color: Colors.lime),
-              currentAccountPicture: CircleAvatar(
-                backgroundColor: Colors.white,
-                child: Image.network(
-                    "https://hermes.digitalinnovation.one/assets/diome/logo.png"),
-              ),
-              accountName: Text("Bruna Midori"),
-              accountEmail: Text("email@email.com")),
+          InkWell(
+            onTap: () {
+              showModalBottomSheet(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                  context: context,
+                  builder: (BuildContext bc) {
+                    return Wrap(
+                      children: [
+                        ListTile(
+                          onTap: () {
+                            Navigator.pop(context);
+                          },
+                          title: const Text("Camera"),
+                          leading: const Icon(Icons.camera),
+                        ),
+                        ListTile(
+                          onTap: () {
+                            Navigator.pop(context);
+                          },
+                          title: const Text("Galeria"),
+                          leading: const Icon(Icons.album),
+                        )
+                      ],
+                    );
+                  });
+            },
+            child: UserAccountsDrawerHeader(
+                decoration: const BoxDecoration(color: Colors.lime),
+                currentAccountPicture: CircleAvatar(
+                  backgroundColor: Colors.white,
+                  child: Image.network(
+                      "https://hermes.digitalinnovation.one/assets/diome/logo.png"),
+                ),
+                accountName: const Text("Bruna Midori"),
+                accountEmail: const Text("email@email.com")),
+          ),
           InkWell(
             child: Container(
                 padding:
@@ -56,7 +84,35 @@ class CustomDrawer extends StatelessWidget {
                     Text("Termos de uso e privacidade"),
                   ],
                 )),
-            onTap: () {},
+            onTap: () {
+              showModalBottomSheet(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                  context: context,
+                  builder: (BuildContext bc) {
+                    return Container(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 16, horizontal: 12),
+                      child: const Column(
+                        children: [
+                          Text(
+                            "Termos de uso e privacidade",
+                            style: TextStyle(
+                                fontSize: 20, fontWeight: FontWeight.bold),
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          Text(
+                            "O empenho em analisar a crescente influência da mídia ainda não demonstrou convincentemente que vai participar na mudança das condições inegavelmente apropriadas. No entanto, não podemos esquecer que a execução dos pontos do programa exige a precisão e a definição das diversas correntes de pensamento",
+                            textAlign: TextAlign.justify,
+                            style: TextStyle(fontSize: 16),
+                          ),
+                        ],
+                      ),
+                    );
+                  });
+            },
           ),
           const Divider(),
           InkWell(
