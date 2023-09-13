8c0b56f81caf0446ebf046a60f40a49806e6b0f8
Bruna_Midori <brunamidoriyy@yahoo.com.br>
2023-09-13 16:54:12 -0300

    async function - linearProgressIndicator

--- lib/pages/card_page.dart
+++ lib/pages/card_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:trilhaapp/model/card_detail.dart';
 import 'package:trilhaapp/pages/card_detail.dart';
+import 'package:trilhaapp/repositories/card_detail_repository.dart';
 
 class CardPage extends StatefulWidget {
   const CardPage({Key? key}) : super(key: key);
@@ -10,12 +11,22 @@ class CardPage extends StatefulWidget {
 }
 
 class _CardPageState extends State<CardPage> {
-  var cardDetail = CardDetail(
-    1,
-    "Meu Card",
-    "https://hermes.digitalinnovation.one/assets/diome/logo.png",
-    "Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado. Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum, e mais recentemente quando passou a ser integrado a softwares de editoração eletrônica como Aldus PageMaker.",
-  );
+  CardDetail? cardDetail;
+  var cardDetailRepository = CardDetailRepository();
+
+  @override
+  void initState() {
+    //initState nao aceita async
+    super.initState();
+    carregarDados();
+  }
+
+  void carregarDados() async {
+    cardDetail = await cardDetailRepository.get();
+    setState(() {
+      // nao aceita async
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,65 +35,68 @@ class _CardPageState extends State<CardPage> {
         Container(
             width: double.infinity,
             margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-            child: InkWell(
-              onTap: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => CardDetailPage(
-                              cardDetail: cardDetail,
-                            )));
-              },
-              //efeito de transição
-              child: Hero(
-                tag: cardDetail.id,
-                child: Card(
-                  elevation: 8,
-                  shadowColor: Colors.grey,
-                  child: Padding(
-                    padding:
-                        const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Row(
-                          children: [
-                            Image.network(
-                              cardDetail.url,
-                              height: 20,
-                            ),
-                            Text(
-                              cardDetail.title,
-                              style: const TextStyle(
-                                  fontSize: 20, fontWeight: FontWeight.w600),
-                            ),
-                          ],
+            child: cardDetail == null
+                ? const LinearProgressIndicator()
+                : InkWell(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => CardDetailPage(
+                                    cardDetail: cardDetail!,
+                                  )));
+                    },
+                    //efeito de transição
+                    child: Hero(
+                      tag: cardDetail!.id,
+                      child: Card(
+                        elevation: 8,
+                        shadowColor: Colors.grey,
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 16, vertical: 8),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Row(
+                                children: [
+                                  Image.network(
+                                    cardDetail!.url,
+                                    height: 20,
+                                  ),
+                                  Text(
+                                    cardDetail!.title,
+                                    style: const TextStyle(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.w600),
+                                  ),
+                                ],
+                              ),
+                              const SizedBox(
+                                height: 10,
+                              ),
+                              Text(
+                                cardDetail!.text,
+                                textAlign: TextAlign.justify,
+                                style: const TextStyle(fontSize: 16),
+                              ),
+                              Container(
+                                width: double.infinity,
+                                alignment: Alignment.centerRight,
+                                child: TextButton(
+                                    onPressed: () {},
+                                    child: const Text(
+                                      "Ler Mais",
+                                      style: TextStyle(
+                                          decoration: TextDecoration.underline),
+                                    )),
+                              )
+                            ],
+                          ),
                         ),
-                        const SizedBox(
-                          height: 10,
-                        ),
-                        Text(
-                          cardDetail.text,
-                          textAlign: TextAlign.justify,
-                          style: const TextStyle(fontSize: 16),
-                        ),
-                        Container(
-                          width: double.infinity,
-                          alignment: Alignment.centerRight,
-                          child: TextButton(
-                              onPressed: () {},
-                              child: const Text(
-                                "Ler Mais",
-                                style: TextStyle(
-                                    decoration: TextDecoration.underline),
-                              )),
-                        )
-                      ],
+                      ),
                     ),
-                  ),
-                ),
-              ),
-            )),
+                  )),
       ],
     );
   }
