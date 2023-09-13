0d97d7759cdea9e7d3d5bb16abf92d51d0e8002e
Bruna_Midori <brunamidoriyy@yahoo.com.br>
2023-09-13 17:34:03 -0300

    lista vertical

--- lib/pages/image_assets.dart
+++ lib/pages/image_assets.dart
@@ -11,33 +11,35 @@ class ImageAssetsPage extends StatefulWidget {
 class _ImageAssetsPageState extends State<ImageAssetsPage> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Image.asset(
-          AppImages.user1,
-          height: 50,
-        ),
-        Image.asset(
-          AppImages.user2,
-          height: 50,
-        ),
-        Image.asset(
-          AppImages.user3,
-          height: 50,
-        ),
-        Image.asset(
-          AppImages.paisagem,
-          width: double.infinity,
-        ),
-        Image.asset(
-          AppImages.paisagem2,
-          height: 50,
-        ),
-        Image.asset(
-          AppImages.paisagem3,
-          height: 50,
-        ),
-      ],
+    return Expanded(
+      child: Column(
+        children: [
+          Image.asset(
+            AppImages.user1,
+            height: 50,
+          ),
+          Image.asset(
+            AppImages.user2,
+            height: 50,
+          ),
+          Image.asset(
+            AppImages.user3,
+            height: 50,
+          ),
+          Image.asset(
+            AppImages.paisagem,
+            width: double.infinity,
+          ),
+          Image.asset(
+            AppImages.paisagem2,
+            width: double.infinity,
+          ),
+          Image.asset(
+            AppImages.paisagem3,
+            height: 50,
+          ),
+        ],
+      ),
     );
   }
 }
